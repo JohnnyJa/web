9533528f89039d1f48175ae99e9e9d4bd8f84ede
Иван Боровков <88048424+JohnnyJa@users.noreply.github.com>
2023-11-18 00:40:23 +0200

    Lab6 added

--- Lab6/api/script.js
+++ Lab6/api/script.js
@@ -6,12 +6,38 @@ function getData(){
         })
     })
 
+    parent = document.getElementById("result");
+
+    child = document.createElement("div");
+
     promise.then((value) => {
         console.log(value);
-        var data = JSON.parse(value);
-            console.log(data.results);
+        img = document.createElement("img");
+        img.src = value.results[0].picture.large;
+        img.width = 250;
+        img.height = 250;
+        child.appendChild(img);
 
-    })
+        city = document.createElement("p");
+        city.appendChild(document.createTextNode("City: "+value.results[0].location.city));
+        child.appendChild(city);    
+
+        uname = document.createElement("p");
+        uname.appendChild(document.createTextNode(
+            "Name: "+ value.results[0].name.title + 
+            " " + value.results[0].name.first + 
+            " " + value.results[0].name.last));
+        child.appendChild(uname); 
+
+        cell = document.createElement("p");
+        cell.appendChild(document.createTextNode(
+            "Cell: "+ value.results[0].cell));
+        child.appendChild(cell);
+
+        phone = document.createElement("p");
+        phone.appendChild(document.createTextNode(
+            "Phone: "+ value.results[0].phone));
+        child.appendChild(phone);
+        parent.appendChild(child);
+    })  
 }
-
-getData();
